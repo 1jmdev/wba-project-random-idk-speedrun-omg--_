c2fcd7977dd9388a08778a467c11b7da16e8ffe9
Matyas Kumzak <me@1jm.dev>
2026-03-30 10:40:06 +0200

    fix lint errors

--- client/src/components/ui/input-otp.tsx
+++ client/src/components/ui/input-otp.tsx
@@ -72,7 +72,6 @@ function InputOTPSeparator({ ...props }: React.ComponentProps<"div">) {
         <div
             data-slot="input-otp-separator"
             className="flex items-center [&_svg:not([class*='size-'])]:size-4"
-            role="separator"
             {...props}
         >
             <MinusIcon />
